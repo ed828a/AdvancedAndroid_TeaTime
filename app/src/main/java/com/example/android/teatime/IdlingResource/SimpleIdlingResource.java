88eef4e3d38bb7127cfa2a0e7e203ccdfb4fccb1
Eddie <ed828a@gmail.com>
2017-11-27 15:15:28 +1030

    implement SimpleIdlingResource class

--- app/src/main/java/com/example/android/teatime/IdlingResource/SimpleIdlingResource.java
+++ app/src/main/java/com/example/android/teatime/IdlingResource/SimpleIdlingResource.java
@@ -1,6 +1,9 @@
 package com.example.android.teatime.IdlingResource;
 
+import android.support.annotation.Nullable;
 import android.support.test.espresso.IdlingResource;
+
+import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
  * Created by Android on 11/27/2017.
@@ -8,4 +11,35 @@ import android.support.test.espresso.IdlingResource;
 
 public class SimpleIdlingResource implements IdlingResource {
 
+    // Idleness is controlled with this boolean.
+    private AtomicBoolean mIsIdleNow = new AtomicBoolean(true);
+
+    @Nullable
+    private volatile ResourceCallback mCallback;
+
+    @Override
+    public String getName() {
+        return this.getClass().getName();
+    }
+
+    @Override
+    public boolean isIdleNow() {
+        return mIsIdleNow.get();
+    }
+
+    @Override
+    public void registerIdleTransitionCallback(ResourceCallback callback) {
+        mCallback = callback;
+    }
+
+    /**
+     * Sets the new idle state, if isIdleNow is true, it pings the {@link ResourceCallback}.
+     * @param isIdleNow false if there are pending operations, true if idle.
+     */
+    public void setIdleState(boolean isIdleNow) {
+        mIsIdleNow.set(isIdleNow);
+        if (isIdleNow && mCallback != null) {
+            mCallback.onTransitionToIdle();
+        }
+    }
 }
