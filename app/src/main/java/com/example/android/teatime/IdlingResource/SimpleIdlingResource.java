054309f75d1cefa3bcbc7ec2dc7d4963b23423d7
Eddie <ed828a@gmail.com>
2017-11-27 14:31:33 +1030

    temp IdlingResource implementation

---
+++ app/src/main/java/com/example/android/teatime/IdlingResource/SimpleIdlingResource.java
@@ -0,0 +1,11 @@
+package com.example.android.teatime.IdlingResource;
+
+import android.support.test.espresso.IdlingResource;
+
+/**
+ * Created by Android on 11/27/2017.
+ */
+
+public class SimpleIdlingResource implements IdlingResource {
+
+}
